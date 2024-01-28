a56959aa8897ddd38b69460d626c5ea792259035
Aryan Abrol <aryanabrol95@gmail.com>
2024-01-28 21:45:17 +0530

    kk

--- express.js
+++ express.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const datajson = fs.readFileSync('data.json','utf-8');
+// const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+// const products = data.products;
+
+// router in express----->
+// const productRouter = express.Router();
+// server.use('/api',productRouter)
+//  for using--> productRouter.get('/products',(req,res)=>{})
+//  for using with modules--> productRouter.get('/products', productRouter.createproduct) createproduct ek function hai jo module mein define hoga
 
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const server = express();
 
 // body parser
@@ -13,18 +21,66 @@ server.use(express.json());
 //   console.log(req.ip,req.method,req.ip,req.hostname,req.get('user-agent'),new Date());
 //   next();
 // })
+// API - Endpoint - Route
 
-server.use(morgan('default'));
-const auth = (req,res,next)=>{
-  // console.log(req.query)
-  if(req.body.password==='123'){
-    next();
-  }
-  else{
-    res.sendStatus(401);
-  }
-}
+// Products
+// API ROOT , base URL, example - google.com/api/v2/
 
+//Create POST /products     C R U D => create,read,update,delete
+// server.post('/products', (req, res) => {
+//   console.log(req.body);
+//   products.push(req.body);
+//   res.status(201).json(req.body);
+// });
+
+
+
+// // Read GET /products
+// server.get('/products', (req, res) => {
+//   res.json(products);
+// });
+
+// // Read GET /products/:id
+// server.get('/products/:id', (req, res) => {
+//   const id = +req.params.id;
+//   const product = products.find(p=>p.id===id)
+//   res.json(product);
+// });
+
+// // Update PUT /products/:id
+// server.put('/products/:id', (req, res) => {
+//   const id = +req.params.id;
+//   const productIndex = products.findIndex(p=>p.id===id)
+//   products.splice(productIndex,1,{...req.body, id:id})
+//   res.status(201).json();
+// });
+// // Update PATCH /products/:id
+// server.patch('/products/:id', (req, res) => {
+//   const id = +req.params.id;
+//   const productIndex = products.findIndex(p=>p.id===id)
+//   const product = products[productIndex];
+//   products.splice(productIndex,1,{...product,...req.body})
+//   res.status(201).json();
+// });
+// // Delete DELETE /products/:id
+// server.delete('/products/:id', (req, res) => {
+//   const id = +req.params.id;
+//   const productIndex = products.findIndex(p=>p.id===id)
+//   const product = products[productIndex];
+//   products.splice(productIndex,1)
+//   res.status(201).json(product);
+// });
+
+// server.use(morgan('default'));
+// const auth = (req,res,next)=>{
+//   // console.log(req.query)
+//   if(req.body.password==='123'){
+//     next();
+//   }
+//   else{
+//     res.sendStatus(401);
+//   }
+// }
 // server.use(auth);
 
 server.get('/product/:id',auth,(req,res)=>
---
+++ mvc.js
@@ -0,0 +1,5 @@
+// basically mvc is model-view-contoller
+// model - matlab kuch rules honge jinpe model banana jayega jaise product ka price jaza nhi hona chahiye
+// view - jo hume front end pe dikhta hai
+// controller - jo control krta hai data koo
+// controller ke liye ek naya folder banao contoller name ka usme file dalo aur modules ki trah hum main file mein import kr skte hai
